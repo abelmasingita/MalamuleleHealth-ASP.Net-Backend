e6c4e8835bd62e511ad187b274f985900534d21e
abel masingita <60102940+abelmasingita@users.noreply.github.com>
2024-01-08 14:31:49 +0200

    2023 last commit

---
+++ DataInterface/Configurations/Dto/Ward/WardDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataInterface.Configurations.Dto.Ward
+{
+    public class WardDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
--- MalamuleleHealth.Web/Controllers/WardController.cs
+++ MalamuleleHealth.Web/Controllers/WardController.cs
@@ -1,4 +1,6 @@
-﻿using DataInterface.Domain;
+﻿using AutoMapper;
+using DataInterface.Configurations.Dto.Ward;
+using DataInterface.Domain;
 using MalamuleleHealth.Application.Repository.IRepository;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -12,93 +14,139 @@ namespace MalamuleleHealth.Web.Controllers
     public class WardController : ControllerBase
     {
         private readonly IUnitofWork unitofWork;
-
-        public WardController(IUnitofWork unitofWork)
+        private readonly IMapper mapper;
+        public WardController(IUnitofWork unitofWork,IMapper mapper)
         {
             this.unitofWork = unitofWork;
+            this.mapper = mapper;
         }
 
-        [HttpGet(Name = "GetWards")]
-        [ProducesResponseType(200, Type = typeof(IEnumerable<Ward>))]
+        [HttpGet("GetWards")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<WardDto>))]
+        [ProducesResponseType(400, Type = typeof(IEnumerable<WardDto>))]
+        [ProducesResponseType(404, Type = typeof(IEnumerable<WardDto>))]
+        [Authorize(Roles = "Administrator, Doctor, Nurse, Pharmacist, LabTechnician")]
         public async Task<IActionResult> GetWards()
         {
-            var wards = unitofWork.Ward.GetList().GetAwaiter().GetResult();
+            try
+            {
+                var wards = unitofWork.Ward.GetList().GetAwaiter().GetResult();
+                var mapped = mapper.Map<WardDto>(wards);
 
-            return Ok(wards);
+                return Ok(mapped);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
+
         }
 
 
-        [HttpGet("wardId")]
-        [ProducesResponseType(200, Type = typeof(Ward))]
-        [ProducesResponseType(400, Type = typeof(Ward))]
-        [ProducesResponseType(404, Type = typeof(Ward))]
+        [HttpGet("GetWard")]
+        [ProducesResponseType(200, Type = typeof(WardDto))]
+        [ProducesResponseType(400, Type = typeof(WardDto))]
+        [ProducesResponseType(404, Type = typeof(WardDto))]
+        [Authorize(Roles = "Administrator, Doctor, Nurse, Pharmacist, LabTechnician")]
         public async Task<IActionResult> GetWard(Guid wardId)
         {
-            var ward = unitofWork.Ward.Get(d => d.Id == wardId).GetAwaiter().GetResult();   
-            if (ward == null)
+
+            try
             {
-                return NotFound();
+                var ward = unitofWork.Ward.Get(d => d.Id == wardId).GetAwaiter().GetResult();
+                if (ward == null)
+                {
+                    return NotFound();
+                }
+
+                var mapped = mapper.Map<WardDto>(ward);
+
+                return Ok(mapped);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
             }
 
-            return Ok(ward);
         }
 
-        [HttpPost]
-        [ProducesResponseType(200, Type = typeof(Ward))]
-        [ProducesResponseType(400, Type = typeof(Ward))]
-        public async Task<IActionResult> AddWard([FromBody] Ward ward)
+        [HttpPost("AddWard")]
+        [ProducesResponseType(200, Type = typeof(WardDto))]
+        [ProducesResponseType(204, Type = typeof(WardDto))]
+        [ProducesResponseType(400, Type = typeof(WardDto))]
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> AddWard([FromBody] AddWardDto ward)
         {
-            if (ward == null)
+
+            try
             {
-                return BadRequest();
+                if (ward == null)
+                {
+                    return BadRequest();
+                }
+
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                var mapped = mapper.Map<Ward>(ward);
+                unitofWork.Ward.Add(mapped);
+                unitofWork.Save();
+
+                return Ok(mapped);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
             }
 
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            unitofWork.Ward.Add(ward);
-            unitofWork.Save();
-
-            return NoContent();
         }
 
 
-        [HttpPut("wardId")]
+        [HttpPut("UpdateWard")]
         [ProducesResponseType(200, Type = typeof(Ward))]
         [ProducesResponseType(400, Type = typeof(Ward))]
+        [Authorize(Roles = "Administrator")]
         public async Task<IActionResult> UpdateWard(Guid wardId,[FromBody] Ward ward)
         {
-            if (ward == null)
-            {
-                return BadRequest();
-            }
 
-            if (!ModelState.IsValid)
+            try
             {
-                return BadRequest(ModelState);
-            }
+                if (ward == null)
+                {
+                    return BadRequest();
+                }
 
-            if (GetWard(wardId).GetAwaiter().GetResult() != null)
-            {
-                unitofWork.Ward.Update(ward);
-                unitofWork.Save();
-            }
-            else
-            {
-                return NotFound();
-            }
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
 
-            return NoContent();
+                if (GetWard(wardId).GetAwaiter().GetResult() != null)
+                {
+                    unitofWork.Ward.Update(ward);
+                    unitofWork.Save();
+                }
+                else
+                {
+                    return NotFound();
+                }
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
         }
 
 
-        [HttpDelete("wardId")]
+        [HttpDelete("RemoveWard")]
         [ProducesResponseType(204)]
         [ProducesResponseType(400)]
         [ProducesResponseType(404)]
-        public async Task<IActionResult> RemoveWard(Guid wardId)
+        public IActionResult RemoveWard(Guid wardId)
         {
             var ward = unitofWork.Ward.Get(d => d.Id == wardId).GetAwaiter().GetResult();
             if (ward == null)
